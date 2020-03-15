567f7c078be4c56637f4a010342f8642127bc79e
Nakib Haider <nakib.haider.protik@gmail.com>
2020-03-15 15:14:33 -0400

    renamed the directory anharmonic to interaction and changed file names within

---
+++ src/interaction/interaction.cpp
@@ -0,0 +1,8 @@
+// * Load necessary header
+// * Call ifc3 reader
+// ** formats: ShengBTE, d3q, phono3py, ...
+// * MPI driver for 3-phonon interaction vertex calculator
+// ** inputs
+// *** memory type: 0 (save to disk), 1 (save to ram)
+// * Test vertex calculator
+
---
+++ src/interaction/ph_interaction_3ph.cpp
@@ -0,0 +1,2 @@
+// * Function to calculate all V_plus/V_minus processes for a given IBZ mode
+// * Function to calculate single V_plus/V_minus
