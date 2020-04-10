cc7e7622398616430b9d728f0b84a3208a8d2269
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-04-10 17:30:24 -0400

    Removing now unused declaration of TransportApp parseArgs

--- src/transport_app.h
+++ src/transport_app.h
@@ -11,6 +11,4 @@ public:
 	 * @param argc, argv: the parameters provided on the command line
 	 */
 	TransportApp(int argc, char** argv);
-private:
-	std::tuple<std::string, std::string> parseInputArgs(int argc, char* argv[]);
 };
