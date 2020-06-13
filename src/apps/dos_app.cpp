002f9806bc7560e6482680f182163ff4ec964bb3
jcoulter12 <jcoulter@g.harvard.edu>
2020-06-13 14:20:28 -0400

    actually pushing the parallel copy of wannierDOS

--- src/apps/dos_app.cpp
+++ src/apps/dos_app.cpp
@@ -52,7 +52,7 @@ void PhononDosApp::run(Context & context) {
 
 // Compute the Electron DOS with tetrahedron method and Fourier interpolation
 void ElectronWannierDosApp::run(Context & context) {
-        std::cout << "Starting electronic DoS calculation" << std::endl;
+        if(mpi->mpiHead()) std::cout << "Starting electronic DoS calculation" << std::endl;
 
         // Read the necessary input files
 
@@ -72,25 +72,47 @@ void ElectronWannierDosApp::run(Context & context) {
         double maxEnergy = context.getDosMaxEnergy();
         double deltaEnergy = context.getDosDeltaEnergy();
         long numEnergies = (maxEnergy - minEnergy) / deltaEnergy + 1;
-        std::vector<double> energies(numEnergies);
-        for ( long i=0; i<numEnergies; i++ ) {
-                energies[i] = i * deltaEnergy + minEnergy;
+
+        // create instructions about how to divide up the work
+        mpi->divideWork(numEnergies);
+        // the amount of values this process has to take care of  
+        int start = mpi->workHead();
+        int stop = mpi->workTail();
+        int workFraction = mpi->workTail() - mpi->workHead();
+
+        std::vector<double> energies(workFraction);
+        // loop over the energies that this process should work on
+        for (int i=start; i<stop; i++ ) {
+                energies[i-start] = i * deltaEnergy + minEnergy;
         }
 
-        // Calculate phonon density of states (DOS) [1/Ry]
-        std::vector<double> dos(numEnergies, 0.); // phonon DOS initialized to zero
-        for ( long i=0; i<numEnergies; i++ ) {
+        // Calculate density of states (DOS) [1/Ry]
+        std::vector<double> dos(workFraction, 0.); // phonon DOS initialized to zero
+        for ( int i=0; i<workFraction; i++ ) {
                 dos[i] += tetrahedra.getDOS(energies[i]);
         }
 
-        // Save phonon DOS to file
-        std::ofstream outfile("./electron_dos.dat");
-        outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
-        for ( long i=0; i<numEnergies; i++ ) {
-                outfile << energies[i] * energyRyToEv << "\t"
-                                << dos[i]/energyRyToEv << "\n";
+        std::vector<double> dosTotal;
+        std::vector<double> eneTotal;
+        // root allocates memory to receive the gathered data 
+        if(mpi->mpiHead()) {
+                dosTotal.resize(numEnergies);
+                eneTotal.resize(numEnergies);
         }
-        std::cout << "Electronic DoS computed" << std::endl;
+        // all processes send their data to be gathered into dos/eneTotal
+        mpi->gatherv(&energies,&eneTotal);
+        mpi->gatherv(&dos,&dosTotal);
+
+        // Merge different ranks and save phonon DOS to file (mpi head only)
+        if(mpi->mpiHead()) {
+                std::ofstream outfile("./electron_dos.dat");
+                outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
+                for ( long i=0; i<numEnergies; i++ ) {
+                        outfile << eneTotal[i] * energyRyToEv << "\t"
+                                         << dosTotal[i]/energyRyToEv << "\n";
+                }
+                std::cout << "Electronic DoS computed" << std::endl;
+        }
 }
 
 // Compute the Electron DOS with tetrahedron method and Fourier interpolation
