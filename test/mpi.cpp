ead8bf9ee529a6119b8a9d47792ab54d2b4171cd
jcoulter12 <jcoulter@g.harvard.edu>
2020-07-08 01:46:10 -0400

    added more mpi tests for tensors, etc

--- test/mpi.cpp
+++ test/mpi.cpp
@@ -8,8 +8,7 @@ TEST(MPITest, AllReduceSum) {
   int size = mpi->getSize();
   int rank = mpi->getRank();
 
-  // test allReduceSum on a std::vector
-
+  // test allReduceSum on a std::vector -------------------------
   std::vector<double> x(size, 0.);
   x[rank] = 1.;
   // the vector has only one element different from zero
@@ -19,8 +18,7 @@ TEST(MPITest, AllReduceSum) {
     EXPECT_EQ(y, 1.);
   }
 
-  // test allReduceSum on a Eigen::MatrixXd
-
+  // test allReduceSum on a Eigen::MatrixXd ---------------------
   Eigen::MatrixXd x2(size,size);
   x2.setZero();
   for ( long i=0; i<size; i++ ) {
@@ -35,13 +33,59 @@ TEST(MPITest, AllReduceSum) {
       }
   }
 
-  std::vector<std::complex<double>> x3(size, complexZero);
-  x3[rank] = complexI;
+  // test allReduceSum on an Eigen::VectorXi ---------------------
+  Eigen::VectorXi x3(size);
+  x3.setZero();
+  for ( long i=0; i<size; i++ ) {
+      x3(rank,i) = 1;
+  }
   // the vector has only one element different from zero
   mpi->allReduceSum(&x3);
   // now the vector should contain 1 everywhere
-  for ( std::complex<double> y : x3 ) {
+  for ( long i=0; i<size; i++ ) {
+      EXPECT_EQ(x3(i), 1);
+  }
+
+  // std::vector test -------------------------------------------
+  std::vector<std::complex<double>> x4(size, complexZero);
+  x4[rank] = complexI;
+  // the vector has only one element different from zero
+  mpi->allReduceSum(&x4);
+  // now the vector should contain 1 everywhere
+  for ( std::complex<double> y : x4 ) {
     EXPECT_EQ(y, complexI);
   }
+
+  // Test allReduce Sum on an Eigen::Tensor ------------------------
+  Eigen::Tensor<double, 5> x5(size,size,size,size,size);
+  x5.setZero();
+
+  // fill every element with one
+  for ( long i=0; i<size; i++ ) {
+        for (long j=0; j<size; j++) {
+                for(long k=0; k<size; k++) {
+                        for(long m=0; m<size; m++) {
+                                for(long n=0; n<size; n++) {
+                                        x5(i,j,k,m,n) = i*j*k*m*n;
+                                } 
+                        }
+                }
+        }
+  }
+  // the vector has only one element different from zero
+  mpi->allReduceSum(&x5);
+  // now the vector should contain nranks everywhere
+  for ( long i=0; i<size; i++ ) {
+        for (long j=0; j<size; j++) {
+                for(long k=0; k<size; k++) {
+                        for(long m=0; m<size; m++) {
+                                for(long n=0; n<size; n++) {
+                                        EXPECT_EQ(x5(i,j,k,m,n), i*k*j*m*n*mpi->getSize());
+                                } 
+                        } 
+                } 
+        }
+  }
+
 
 }
