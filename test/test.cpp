a28cc8ed04a9c0b204c35b94aa295a46b94b04a4
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-07-09 09:54:46 -0400

    Adding initBlacs call also in the test suite.

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,15 @@
 #include "example.h"
 #include "gtest/gtest.h"
 #include "mpiHelper.h"
+#include "context.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
   initMPI();
+
+  Context context;
+  mpi->initBlacs(context);
 
   int errCode = RUN_ALL_TESTS();
 
