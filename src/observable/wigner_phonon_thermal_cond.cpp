d120eb717482622a27dd19d36445df1f78231b3f
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-07-14 10:23:46 -0400

    The Wigner function was changing the values of the phonon relaxation times, affecting the results of viscosity.

--- src/observable/wigner_phonon_thermal_cond.cpp
+++ src/observable/wigner_phonon_thermal_cond.cpp
@@ -8,15 +8,8 @@ WignerPhononThermalConductivity::WignerPhononThermalConductivity(
     StatisticsSweep &statisticsSweep_, Crystal &crystal_,
     BaseBandStructure &bandStructure_, VectorBTE &relaxationTimes)
     : PhononThermalConductivity(statisticsSweep_, crystal_, bandStructure_),
-      linewidths(relaxationTimes) {
+      smaRelTimes(relaxationTimes) {
   int numCalcs = statisticsSweep.getNumCalcs();
-
-  // since we pass in input the relaxation times, we invert them here
-  for (int iCalc = 0; iCalc < numCalcs; iCalc++) {
-    for (int is = 0; is < bandStructure.getNumStates(); is++) {
-      linewidths.data(iCalc, is) = 1. / linewidths.data(iCalc, is);
-    }
-  }
 
   wignerCorrection =
       Eigen::Tensor<double, 3>(numCalcs, dimensionality, dimensionality);
@@ -70,11 +63,11 @@ WignerPhononThermalConductivity::WignerPhononThermalConductivity(
                       .real();
               double den =
                   4. * pow(energies(ib1) - energies(ib2), 2) +
-                  pow(linewidths.data(iCalc, is1) + linewidths.data(iCalc, is2),
+                  pow(1./smaRelTimes.data(iCalc, is1) + 1./smaRelTimes.data(iCalc, is2),
                       2);
               wignerCorrection(iCalc, ic1, ic2) +=
                   (energies(ib1) + energies(ib2)) * vel * num / den *
-                  (linewidths.data(iCalc, is1) + linewidths.data(iCalc, is2)) *
+                  (1./smaRelTimes.data(iCalc, is1) + 1./smaRelTimes.data(iCalc, is2)) *
                   norm(iCalc);
             }
           }
@@ -89,7 +82,7 @@ WignerPhononThermalConductivity::WignerPhononThermalConductivity(
 WignerPhononThermalConductivity::WignerPhononThermalConductivity(
     const WignerPhononThermalConductivity &that)
     : PhononThermalConductivity(that),
-      linewidths(that.linewidths),
+      smaRelTimes(that.smaRelTimes),
       wignerCorrection(that.wignerCorrection) {}
 
 // copy assigmnent
@@ -97,7 +90,7 @@ WignerPhononThermalConductivity &WignerPhononThermalConductivity::operator=(
     const WignerPhononThermalConductivity &that) {
   PhononThermalConductivity::operator=(that);
   if (this != &that) {
-    linewidths = that.linewidths;
+    smaRelTimes = that.smaRelTimes;
     wignerCorrection = that.wignerCorrection;
   }
   return *this;
