2c817950ea3afda59e2eda0a43335540d92629c7
jcoulter12 <jcoulter@g.harvard.edu>
2020-10-13 16:55:11 -0400

    use helper function to remove code reuse in dos calculation in dosApp

--- src/apps/dos_app.cpp
+++ src/apps/dos_app.cpp
@@ -13,6 +13,11 @@
 #include "qe_input_parser.h"
 #include "utilities.h"
 
+// Forward declare this helper function so that 
+// app::run functions can remain at the top 
+std::tuple<std::vector<double>, std::vector<double>> calcDOS(
+      Context& context, FullBandStructure& fullBandStructure);
+
 // Compute the DOS with the tetrahedron method
 void PhononDosApp::run(Context &context) {
   if (mpi->mpiHead()) {
@@ -31,6 +36,96 @@ void PhononDosApp::run(Context &context) {
   FullBandStructure fullBandStructure =
       phononH0.populate(fullPoints, withVelocities, withEigenvectors);
 
+  // calculate DOS via the tetrahedron method 
+  auto tup1 = calcDOS(context, fullBandStructure);
+  std::vector<double> energies = std::get<0>(tup1);
+  std::vector<double> dos = std::get<1>(tup1);
+
+  // Save phonon DOS to file
+  if (mpi->mpiHead()) {
+    std::ofstream outfile("./phonon_dos.dat");
+    outfile << "# Phonon density of states: frequency[Cmm1], Dos[1/Ry]\n";
+    for (unsigned int i = 0; i < energies.size(); i++) {
+      outfile << energies[i] * ryToCmm1 << "\t" << dos[i] << "\n";
+    }
+    std::cout << "Phonon DoS computed." << std::endl;
+  }
+}
+
+// Compute the Electron DOS with tetrahedron method and Wannier interpolation
+void ElectronWannierDosApp::run(Context &context) {
+  if (mpi->mpiHead())
+    std::cout << "Starting electronic (Wannier) DoS calculation" << std::endl;
+
+  // Read the necessary input files
+  auto tup = QEParser::parseElHarmonicWannier(context);
+  auto crystal = std::get<0>(tup);
+  auto h0 = std::get<1>(tup);
+
+  // first we make compute the band structure on the fine grid
+  FullPoints fullPoints(crystal, context.getKMesh());
+  bool withVelocities = false;
+  bool withEigenvectors = false;
+  FullBandStructure fullBandStructure =
+      h0.populate(fullPoints, withVelocities, withEigenvectors);
+
+  // calculate DOS via the tetrahedron method 
+  auto tup1 = calcDOS(context, fullBandStructure);
+  std::vector<double> energies = std::get<0>(tup1);
+  std::vector<double> dos = std::get<1>(tup1);
+
+  // Merge different ranks and save DOS to file (mpi head only)
+  if (mpi->mpiHead()) {
+    std::ofstream outfile("./electron_dos.dat");
+    outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
+    for (unsigned int i = 0; i < energies.size(); i++) {
+      outfile << energies[i] * energyRyToEv << "\t"
+              << dos[i] / energyRyToEv << "\n";
+    }
+    std::cout << "Electronic (Wannier) DoS computed" << std::endl;
+  }
+}
+
+// Compute the Electron DOS with tetrahedron method and Fourier interpolation
+void ElectronFourierDosApp::run(Context &context) {
+  if (mpi->mpiHead())
+    std::cout << "Starting electronic (Fourier) DoS calculation" << std::endl;
+
+  // Read the necessary input files
+  auto tup = QEParser::parseElHarmonicFourier(context);
+  auto crystal = std::get<0>(tup);
+  auto h0 = std::get<1>(tup);
+
+  // first we make compute the band structure on the fine grid
+  FullPoints fullPoints(crystal, context.getKMesh());
+  bool withVelocities = false;
+  bool withEigenvectors = false;
+  FullBandStructure fullBandStructure =
+      h0.populate(fullPoints, withVelocities, withEigenvectors);
+
+  // calculate DOS via the tetrahedron method 
+  auto tup1 = calcDOS(context, fullBandStructure); 
+  std::vector<double> energies = std::get<0>(tup1); 
+  std::vector<double> dos = std::get<1>(tup1);
+
+  // save dos to an output file
+  if (mpi->mpiHead()) {
+    std::ofstream outfile("./electron_dos.dat");
+    outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
+    for (unsigned int i = 0; i < energies.size(); i++) {
+      outfile << energies[i] * energyRyToEv << "\t"
+              << dos[i] / energyRyToEv << "\n";
+    }
+    std::cout << "Electronic (Fourier) DoS computed" << std::endl;
+  }
+}
+
+/* 
+ * Generic helper function to calculate DoS via tetrahedron method 
+ */ 
+std::tuple<std::vector<double>, std::vector<double>> calcDOS(
+        Context& context, FullBandStructure& fullBandStructure) { 
+
   // Form tetrahedra and fill them with eigenvalues
   TetrahedronDeltaFunction tetrahedra(fullBandStructure);
 
@@ -47,148 +142,14 @@ void PhononDosApp::run(Context &context) {
   int workFraction = stop - start;
 
   std::vector<double> energies(workFraction);
-  for (long i = start; i < stop; i++) {
-    energies[i - start] = i * deltaEnergy;
-  }
-
-  // Calculate phonon density of states (DOS) [1/Ry]
-  std::vector<double> dos(workFraction, 0.);  // phonon DOS initialized to zero
-  for (long i = 0; i < workFraction; i++) {
-    dos[i] += tetrahedra.getDOS(energies[i]);
-  }
-
-  // containers for the final result, only allocated for head process
-  std::vector<double> dosTotal;
-  std::vector<double> eneTotal;
-  // root allocates memory to receive the gathered data
-  if (mpi->mpiHead()) {
-    dosTotal.resize(numEnergies);
-    eneTotal.resize(numEnergies);
-  }
-
-  // all processes send their data to be gathered into dos/eneTotal
-  mpi->gatherv(&energies, &eneTotal);
-  mpi->gatherv(&dos, &dosTotal);
-
-  // Save phonon DOS to file
-  if (mpi->mpiHead()) {
-    std::ofstream outfile("./phonon_dos.dat");
-    outfile << "# Phonon density of states: frequency[Cmm1], Dos[1/Ry]\n";
-    for (long i = 0; i < numEnergies; i++) {
-      outfile << eneTotal[i] * ryToCmm1 << "\t" << dosTotal[i] << "\n";
-    }
-    std::cout << "Phonon DoS computed" << std::endl;
-  }
-}
-
-// Compute the Electron DOS with tetrahedron method and Fourier interpolation
-void ElectronWannierDosApp::run(Context &context) {
-  if (mpi->mpiHead())
-    std::cout << "Starting electronic DoS calculation" << std::endl;
-
-  // Read the necessary input files
-  auto tup = QEParser::parseElHarmonicWannier(context);
-  auto crystal = std::get<0>(tup);
-  auto h0 = std::get<1>(tup);
-
-  // first we make compute the band structure on the fine grid
-  FullPoints fullPoints(crystal, context.getKMesh());
-  bool withVelocities = false;
-  bool withEigenvectors = false;
-  FullBandStructure fullBandStructure =
-      h0.populate(fullPoints, withVelocities, withEigenvectors);
-
-  // Form tetrahedra and fill them with eigenvalues
-  TetrahedronDeltaFunction tetrahedra(fullBandStructure);
-
-  double minEnergy = context.getDosMinEnergy();
-  double maxEnergy = context.getDosMaxEnergy();
-  double deltaEnergy = context.getDosDeltaEnergy();
-  long numEnergies = (maxEnergy - minEnergy) / deltaEnergy + 1;
-
-  // create instructions about how to divide up the work
-  auto divs = mpi->divideWork(numEnergies);
-  // the amount of values this process has to take care of
-  int start = divs[0]; 
-  int stop = divs[1];
-  int workFraction = stop - start;
-
-  std::vector<double> energies(workFraction);
-// loop over the energies that this process should work on
-#pragma omp parallel for
-  for (int i = start; i < stop; i++) {
-    energies[i - start] = i * deltaEnergy + minEnergy;
-  }
-
-  // Calculate density of states (DOS) [1/Ry], for energies on this process
-  std::vector<double> dos(workFraction, 0.);  // DOS initialized to zero
-#pragma omp parallel for
-  for (int i = 0; i < workFraction; i++) {
-    dos[i] += tetrahedra.getDOS(energies[i]);
-  }
-
-  std::vector<double> dosTotal;
-  std::vector<double> eneTotal;
-  // root allocates memory to receive the gathered data
-  if (mpi->mpiHead()) {
-    dosTotal.resize(numEnergies);
-    eneTotal.resize(numEnergies);
-  }
-  // all processes send their data to be gathered into dos/eneTotal
-  mpi->gatherv(&energies, &eneTotal);
-  mpi->gatherv(&dos, &dosTotal);
-
-  // Merge different ranks and save DOS to file (mpi head only)
-  if (mpi->mpiHead()) {
-    std::ofstream outfile("./electron_dos.dat");
-    outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
-    for (long i = 0; i < numEnergies; i++) {
-      outfile << eneTotal[i] * energyRyToEv << "\t"
-              << dosTotal[i] / energyRyToEv << "\n";
-    }
-    std::cout << "Electronic DoS computed" << std::endl;
-  }
-}
-
-// Compute the Electron DOS with tetrahedron method and Fourier interpolation
-void ElectronFourierDosApp::run(Context &context) {
-  if (mpi->mpiHead())
-    std::cout << "Starting electronic DoS calculation" << std::endl;
-
-  // Read the necessary input files
-  auto tup = QEParser::parseElHarmonicFourier(context);
-  auto crystal = std::get<0>(tup);
-  auto h0 = std::get<1>(tup);
-
-  // first we make compute the band structure on the fine grid
-  FullPoints fullPoints(crystal, context.getKMesh());
-  bool withVelocities = false;
-  bool withEigenvectors = false;
-  FullBandStructure fullBandStructure =
-      h0.populate(fullPoints, withVelocities, withEigenvectors);
-
-  // Form tetrahedra and fill them with eigenvalues
-  TetrahedronDeltaFunction tetrahedra(fullBandStructure);
-
-  double minEnergy = context.getDosMinEnergy();
-  double maxEnergy = context.getDosMaxEnergy();
-  double deltaEnergy = context.getDosDeltaEnergy();
-  long numEnergies = (maxEnergy - minEnergy) / deltaEnergy + 1;
-
-  // create instructions about how to divide up the work
-  auto divs = mpi->divideWork(numEnergies);
-  // the amount of values this process has to take care of
-  int start = divs[0]; 
-  int stop = divs[1];
-  int workFraction = stop - start;
-
-  std::vector<double> energies(workFraction);
+  #pragma omp parallel for
   for (long i = start; i < stop; i++) {
     energies[i - start] = i * deltaEnergy + minEnergy;
   }
 
   // Calculate phonon density of states (DOS) [1/Ry]
   std::vector<double> dos(workFraction, 0.);  // DOS initialized to zero
+  #pragma omp parallel for
   for (long i = 0; i < workFraction; i++) {
     dos[i] += tetrahedra.getDOS(energies[i]);
   }
@@ -203,18 +164,9 @@ void ElectronFourierDosApp::run(Context &context) {
   // all processes send their data to be gathered into dos/eneTotal
   mpi->gatherv(&energies, &eneTotal);
   mpi->gatherv(&dos, &dosTotal);
-
-  // save dos to an output file
-  if (mpi->mpiHead()) {
-    std::ofstream outfile("./electron_dos.dat");
-    outfile << "# Electronic density of states: energy[eV], Dos[1/Ry]\n";
-    for (long i = 0; i < numEnergies; i++) {
-      outfile << eneTotal[i] * energyRyToEv << "\t"
-              << dosTotal[i] / energyRyToEv << "\n";
-    }
-    std::cout << "Electronic DoS computed" << std::endl;
-  }
+  return {eneTotal, dosTotal}; 
 }
+
 void PhononDosApp::checkRequirements(Context &context) {
   throwErrorIfUnset(context.getPhD2FileName(), "PhD2FileName");
   throwErrorIfUnset(context.getQMesh(), "qMesh");
