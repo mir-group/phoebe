17f489495e6b3fceba431cbfbbff174be476106a
jcoulter12 <jcoulter@g.harvard.edu>
2020-10-14 13:42:23 -0400

    plot scripts for bands and dos from json

---
+++ scripts/bands.py
@@ -0,0 +1,35 @@
+import json 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load in the json output
+# change to phonon_bands.json, the same script works for phonons
+jfile = open('electron_bands.json') 
+data = json.load(jfile)
+
+# unpack the json file
+points = np.array(data['pathIndices'])
+numBands = data['numBands'] 
+energies = np.array(data['energies'])
+mu = data['chemicalPotential']
+pathLabels = data['highSymLabels']
+pathTicks = data['highSymIndices']
+
+# plot some vertical lines at high sym points
+for i in pathTicks: 
+        plt.axvline(i, color='grey')
+
+# plot chemical potential 
+if(data['particleType'] == "electron"): 
+        plt.axhline(0, color='grey', ls='--')
+
+# plot the bands
+for i in range(numBands): 
+        plt.plot(points, energies[:,i] - mu, lw=2, color='cornflowerblue')
+
+plt.xticks(pathTicks,pathLabels)
+plt.ylabel(r'Energy [' + data['energyUnit'] +']')
+plt.ylim(None, None)
+plt.xlim(points[0],points[-1])
+
+plt.show()
---
+++ scripts/dos.py
@@ -0,0 +1,29 @@
+import json 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load in the json output
+#jfile = open('phonon_dos.json') # works for phonons too 
+jfile = open('electron_dos.json')
+data = json.load(jfile)
+
+# unpack the json file
+energies = np.array(data['energies'])
+dos = np.array(data['dos'])
+mu = data['chemicalPotential']
+
+# plot chemical potential 
+plt.figure(figsize=(5,3))
+if(data['particleType'] == "electron"): 
+        plt.axvline(0, color='grey', ls='--')
+
+# plot the bands
+plt.plot(energies - mu, dos, lw=2, color='cornflowerblue')
+
+# plot aesthetics
+plt.xlabel(r'Energy [' + data['energyUnit'] +']')
+plt.ylabel(r'DoS [' + data['dosUnit'] + ']')
+plt.xlim(np.min(energies),np.max(energies))
+plt.ylim(None, np.max(dos)+np.max(dos)*0.1)
+
+plt.show()
