72b94a0c2ae4c6a49e1374879d06438247a0aa49
jcoulter12 <jcoulter@g.harvard.edu>
2020-10-16 11:27:02 -0400

    update bands.py to work with new fermiLevel outputs

--- scripts/bands.py
+++ scripts/bands.py
@@ -11,24 +11,33 @@ data = json.load(jfile)
 points = np.array(data['wavevectorIndices'])
 numBands = data['numBands']
 energies = np.array(data['energies'])
-mu = data['chemicalPotential']
 pathLabels = data['highSymLabels']
 pathTicks = data['highSymIndices']
 
 # plot some vertical lines at high sym points
+plt.figure(figsize=(5.5,5))
 for i in pathTicks:
         plt.axvline(i, color='grey')
 
-# plot chemical potential
-if(data['particleType'] == "electron"):
+# if fermiLevel as set in the input file,
+# we can also read it in and plot it
+if('fermiLevel' in data):
+        mu = data['fermiLevel']
         plt.axhline(0, color='grey', ls='--')
+        energyLabel = r'E-E$_F$ [' + data['energyUnit'] +']'
+# if it wasn't set, we won't subtract
+# anything from the band energies
+else:
+        mu = 0
+        energyLabel = r'Energy [' + data['energyUnit'] +']'
 
 # plot the bands
 for i in range(numBands):
         plt.plot(points, energies[:,i] - mu, lw=2, color='royalblue')
 
-plt.xticks(pathTicks,pathLabels)
-plt.ylabel(r'Energy [' + data['energyUnit'] +']')
+plt.xticks(pathTicks,pathLabels,fontsize=12)
+plt.yticks(fontsize=12)
+plt.ylabel(energyLabel,fontsize=14)
 plt.ylim(None, None)
 plt.xlim(points[0],points[-1])
 
