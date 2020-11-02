fe6db4202a076a90c9906ffa2f4f686275f19679
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-11-02 16:21:23 -0500

    Updated the electron-phonon example for silicon.

--- example/Silicon-el/qe-elph/runMe.sh
+++ example/Silicon-el/qe-elph/runMe.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
-# Note: QE executables come from the patched version of QE
-# Wannier90 is unmodified
+export QE_PATH="/your/path/to/patched/phoebe-quantum-espresso/bin"
+export NMPI=4
+export NPOOL=4
 
-mpirun -np 4 pw.x -npool 4 -in scf.in > scf.out
-mpirun -np 4 ph.x -npool 4 -in ph.in > ph.out
-q2r.x -in q2r.in > q2r.out
+# Note: must point to the patched version of QE
+# (you can use an unmodified version of Wannier90)
 
-#mpirun -np 1 pw.x -npool 1 -in scf.in > scf.out
-#mpirun -np 1 pw.x -npool 1 -in nscf.in > nscf.out
-#~/software/wannier90-3.0.0/wannier90.x -pp si
-#mpirun -np 1 pw2wannier90.x -in pw2wan.in > pw2wan.out
-#~/software/wannier90-3.0.0/wannier90.x si
+mpirun -np $NMPI $QE_PATH/pw.x -npool $NPOOL -in scf.in > scf.out
+mpirun -np $NMPI $QE_PATH/ph.x -npool $NPOOL -in ph.in > ph.out
+$QE_PATH/q2r.x -in q2r.in > q2r.out
 
-#mpirun -np 4 pw.x -npool 4 -in scf.in > scf.out
-#mpirun -np 4 pw.x -npool 4 -in nscf.in > nscf.out
-#~/software/wannier90-3.0.0/wannier90.x -pp si
-#mpirun -np 4 pw2wannier90.x -in pw2wan.in > pw2wan.out
-#~/software/wannier90-3.0.0/wannier90.x si
-
-#mpirun -np 4 ~/git/q-e/bin/pw.x -npool 4 -in scf.in > scf.out
-#mpirun -np 4 ~/git/q-e/bin/pw.x -npool 4 -in nscf.in > nscf.out
-#~/software/wannier90-3.0.0/wannier90.x -pp si
-#mpirun -np 4 ~/git/q-e/bin/pw2wannier90.x -in pw2wan.in > pw2wan.out
-#~/software/wannier90-3.0.0/wannier90.x si
+mpirun -np $NMPI $QE_PATH/pw.x -npool $NPOOL -in scf.in > scf.out
+mpirun -np $NMPI $QE_PATH/pw.x -npool $NPOOL -in nscf.in > nscf.out
+$QE_PATH/wannier90.x -pp si
+mpirun -np $NMPI $QE_PATH/pw2wannier90.x -in pw2wan.in > pw2wan.out
+$QE_PATH/wannier90.x si
