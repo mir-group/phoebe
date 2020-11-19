c8648ada32add3e79d36fc71bf074075e13ba063
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-11-19 10:22:21 -0500

    Simplifying the drift operator construction.

--- src/bte/drift.cpp
+++ src/bte/drift.cpp
@@ -9,21 +9,14 @@ BulkTDrift::BulkTDrift(StatisticsSweep &statisticsSweep_,
   Particle particle = bandStructure.getParticle();
   for (long is = 0; is < numStates; is++) {
     double energy = bandStructure.getEnergy(is);
-    Eigen::Vector3d velocity = bandStructure.getGroupVelocity(is);
-
-    for (long iCalc = 0; iCalc < numCalcs; iCalc++) {
-      auto tup = loc2Glob(iCalc);
-      auto imu = std::get<0>(tup);
-      auto it = std::get<1>(tup);
-      auto idim = std::get<2>(tup);
-      int idimIndex = idim.get();
-      double vel = velocity(idimIndex);
-      auto calcStat = statisticsSweep.getCalcStatistics(it, imu);
+    Eigen::Vector3d vel = bandStructure.getGroupVelocity(is);
+    for (long iCalc = 0; iCalc < statisticsSweep.getNumCalcs(); iCalc++) {
+      auto calcStat = statisticsSweep.getCalcStatistics(iCalc);
       auto chemicalPotential = calcStat.chemicalPotential;
       auto temperature = calcStat.temperature;
-
-      double x = particle.getDndt(energy, temperature, chemicalPotential) * vel;
-      data(iCalc, is) = x;
+      for (int i : {0,1,2}) {
+        operator()(iCalc, i, is) = particle.getDndt(energy, temperature, chemicalPotential) * vel(i);
+      }
     }
   }
 }
@@ -35,21 +28,14 @@ BulkEDrift::BulkEDrift(StatisticsSweep &statisticsSweep_,
   Particle particle = bandStructure.getParticle();
   for (long is = 0; is < numStates; is++) {
     double energy = bandStructure.getEnergy(is);
-    Eigen::Vector3d velocity = bandStructure.getGroupVelocity(is);
-
-    for (long iCalc = 0; iCalc < numCalcs; iCalc++) {
-      auto tup = loc2Glob(iCalc);
-      auto imu = std::get<0>(tup);
-      auto it = std::get<1>(tup);
-      auto idim = std::get<2>(tup);
-      int idimIndex = idim.get();
-      double vel = velocity(idimIndex);
-      auto calcStat = statisticsSweep.getCalcStatistics(it, imu);
+    Eigen::Vector3d vel = bandStructure.getGroupVelocity(is);
+    for (long iCalc = 0; iCalc < statisticsSweep.getNumCalcs(); iCalc++) {
+      auto calcStat = statisticsSweep.getCalcStatistics(iCalc);
       auto chemicalPotential = calcStat.chemicalPotential;
       auto temperature = calcStat.temperature;
-
-      double x = particle.getDnde(energy, temperature, chemicalPotential) * vel;
-      data(iCalc, is) = -x;
+      for (int i : {0,1,2}) {
+        operator()(iCalc, i, is) = -particle.getDnde(energy, temperature, chemicalPotential) * vel(i);
+      }
     }
   }
 }
@@ -57,25 +43,18 @@ BulkEDrift::BulkEDrift(StatisticsSweep &statisticsSweep_,
 Vector0::Vector0(StatisticsSweep &statisticsSweep_,
                  BaseBandStructure &bandStructure_, SpecificHeat &specificHeat)
     : VectorBTE(statisticsSweep_, bandStructure_, 1) {
-  data.setZero();
-
   Particle particle = bandStructure.getParticle();
-
-  for (long iCalc = 0; iCalc < numCalcs; iCalc++) {
-    auto tup = loc2Glob(iCalc);
-    auto imu = std::get<0>(tup);
-    auto it = std::get<1>(tup);
-    auto calcStat = statisticsSweep.getCalcStatistics(it, imu);
-    double temp = calcStat.temperature;
-    double chemPot = calcStat.chemicalPotential;
-
-    for (long is = 0; is < numStates; is++) {
-      double energy = bandStructure.getEnergy(is);
+  for (long is = 0; is < numStates; is++) {
+    double energy = bandStructure.getEnergy(is);
+    for (long iCalc = 0; iCalc < statisticsSweep.getNumCalcs(); iCalc++) {
+      auto calcStat = statisticsSweep.getCalcStatistics(iCalc);
+      double temp = calcStat.temperature;
+      double chemPot = calcStat.chemicalPotential;
       double dnde = particle.getDnde(energy, temp, chemPot);
       // note dnde = n(n+1)/T  (for bosons)
-      auto c = specificHeat.get(imu, it);
+      auto c = specificHeat.get(iCalc);
       double x = -dnde / temp / c;
-      data(iCalc, is) += std::sqrt(x) * energy;
+      operator()(iCalc, 0, is) += std::sqrt(x) * energy;
       // we use std::sqrt because we overwrote sqrt() in the base class
     }
   }
