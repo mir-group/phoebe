c22f3897595f3a2f6cd65f99a4ff72bd34fb4c16
Andrea Cepellotti <andrea.cepellotti@gmail.com>
2020-12-17 11:47:43 -0500

    Temptative conversion of phono3py D3 format to phoebe's

--- src/parser/ifc3_parser.cpp
+++ src/parser/ifc3_parser.cpp
@@ -39,7 +39,7 @@ Interaction3Ph IFC3Parser::parse(Context &context, Crystal &crystal) {
     //}
 }
 
-long findIndexRow(Eigen::MatrixXd &cellPositions2, Eigen::Vector3d &position2) {
+long IFC3Parser::findIndexRow(Eigen::MatrixXd &cellPositions2, Eigen::Vector3d &position2) {
   long ir2 = -1;
   for (int i = 0; i < cellPositions2.cols(); i++) {
     if ((position2 - cellPositions2.col(i)).norm() < 1.e-12) {
@@ -53,12 +53,83 @@ long findIndexRow(Eigen::MatrixXd &cellPositions2, Eigen::Vector3d &position2) {
   return ir2;
 }
 
+std::tuple<Eigen::MatrixXd, Eigen::VectorXi> IFC3Parser::buildWignerSeitz(
+    const Eigen::MatrixXd &supBravaisVectors,
+    Crystal &crystal, Eigen::Vector3i grid) {
+
+  // TODO: think if 2 is enough?
+  int nx = 2;
+
+  Eigen::Matrix3d directUnitCell = crystal.getDirectUnitCell();
+  Eigen::Vector3d a0 = crystal.col(0);
+  Eigen::Vector3d a1 = crystal.col(1);
+  Eigen::Vector3d a2 = crystal.col(2);
+  Eigen::Vector3d aSup0 = a0 * grid(0);
+  Eigen::Vector3d aSup1 = a1 * grid(1);
+  Eigen::Vector3d aSup2 = a2 * grid(2);
+
+  std::vector<Eigen::Vector3d> tmpVectors;
+  std::vector<int> tmpWSMap;
+
+  for (int iSup=0; iSup<supBravaisVectors.rows(); iSup++) {
+    Eigen::Vector3d thisBV = supBravaisVectors.col(iSup);
+
+    std::vector<double> distances;
+    std::vector<Eigen::Vector3d> shiftedBVs;
+    for (int j0 = -nx - 1; j0 <= nx + 1; j0++) {
+      for (int j1 = -nx - 1; j1 <= nx + 1; j1++) {
+        for (int j2 = -nx - 1; j2 <= nx + 1; j2++) {
+          Eigen::Vector3d shiftedBV = thisBV +
+                                      j0 * aSup0 + j1 * aSup1 + j2 * aSup2;
+          distances.push_back(shiftedBV.squaredNorm());
+          shiftedBVs.push_back(shiftedBV);
+        }
+      }
+    }
+
+    // find the minimum distance out of all distances
+    double distMin = distances[0]; // [0] is just the first guess
+    for (double dist : distances) {
+      if (dist < distMin) {
+        distMin = dist;
+      }
+    }
+
+    assert(distances.size() == shiftedBVs.size());
+    for (int i=0; i<shiftedBVs.size(); i++) {
+      double dist = distances[i];
+      if ( pow(dist-distMin,2) < 1.0e-5 ) {
+        // we have the minimum here
+        tmpVectors.push_back(shiftedBVs[i]);
+        tmpWSMap.push_back(iSup);
+      }
+    }
+
+  }
+
+  // now we store the list of these lattice vectors in the class members
+  int numPositionVectors = tmpVectors.size();
+  Eigen::MatrixXd positionVectors(3, numPositionVectors);
+  Eigen::VectorXi wsMap(numPositionVectors);
+  wsMap.setZero();
+  positionVectors.setZero();
+  for (int iR = 0; iR < numPositionVectors; iR++) {
+    positionVectors.col(iR) = tmpVectors[iR];
+    wsMap(iR) = tmpWSMap[iR];
+  }
+
+  return {positionVectors, wsMap};
+}
+
+
+
 Interaction3Ph IFC3Parser::parseFromPhono3py(Context &context, Crystal &crystal) {
 
 #ifndef HDF5_AVAIL
 
-  Error e("Phono3py HDF5 output cannot be read if Phoebe is not built with HDF5.");
-  //return void;
+  Error e(
+      "Phono3py HDF5 output cannot be read if Phoebe is not built with HDF5.");
+  // return void;
 
 #else
 
@@ -71,8 +142,8 @@ Interaction3Ph IFC3Parser::parseFromPhono3py(Context &context, Crystal &crystal)
   // the information we need outside the ifcs3 is in a file called
   // disp_fc3.yaml, which contains the atomic positions of the
   // supercell and the displacements of the atoms
-  // The mapping between the supercell and unit cell atoms is 
-  // set up so that the first nAtoms*dimSup of the supercell are unit cell 
+  // The mapping between the supercell and unit cell atoms is
+  // set up so that the first nAtoms*dimSup of the supercell are unit cell
   // atom #1, and the second nAtoms*dimSup are atom #2, and so on.
 
   // First, read in the information form disp_fc3.yaml
@@ -85,42 +156,43 @@ Interaction3Ph IFC3Parser::parseFromPhono3py(Context &context, Crystal &crystal)
   std::ifstream infile("disp_fc3.yaml");
   std::string line;
   if (not infile.is_open()) {
-      Error e("Phono3py disp_fc3.yaml file not found");
+    Error e("Phono3py disp_fc3.yaml file not found");
   }
 
   // first line will always be natoms in supercell
   std::getline(infile, line);
-  int numSupAtoms = std::stoi(line.substr(line.find(" ") ,line.back()));
-  int nr = numSupAtoms/numAtoms;
+  int numSupAtoms = std::stoi(line.substr(line.find(" "), line.back()));
+  int numSupCells = numSupAtoms / numAtoms;
 
   // read the rest of the file to look for supercell positions
   Eigen::MatrixXd supPositions(numSupAtoms, 3);
   Eigen::MatrixXd lattice(3, 3);
   int ilatt = 3;
   int ipos = 0;
-  while(infile) {
+  while (infile) {
     getline(infile, line);
     // if this is a cell position, save it
-    if(line.find("position: ") != std::string::npos) {
-      std::string temp = line.substr(14,57); // just the positions
+    if (line.find("position: ") != std::string::npos) {
+      std::string temp = line.substr(14, 57); // just the positions
       int idx1 = temp.find(",");
-      supPositions(ipos,0) = std::stod(temp.substr(0,idx1));
-      int idx2 = temp.find(",", idx1+1);
-      supPositions(ipos,1) = std::stod(temp.substr(idx1+1,idx2));
-      supPositions(ipos,2) = std::stod(temp.substr(idx2+1));
+      supPositions(ipos, 0) = std::stod(temp.substr(0, idx1));
+      int idx2 = temp.find(",", idx1 + 1);
+      supPositions(ipos, 1) = std::stod(temp.substr(idx1 + 1, idx2));
+      supPositions(ipos, 2) = std::stod(temp.substr(idx2 + 1));
       ipos++;
     }
-    if(ilatt < 3) { // count down lattice lines 
+    if (ilatt < 3) { // count down lattice lines
       // convert from angstrom to bohr
-      std::string temp = line.substr(5,62); // just the elements
+      std::string temp = line.substr(5, 62); // just the elements
       int idx1 = temp.find(",");
-      lattice(ilatt,0) = std::stod(temp.substr(0,idx1))/distanceBohrToAng;
-      int idx2 = temp.find(",", idx1+1);
-      lattice(ilatt,1) = std::stod(temp.substr(idx1+1,idx2))/distanceBohrToAng;
-      lattice(ilatt,2) = std::stod(temp.substr(idx2+1))/distanceBohrToAng;      
+      lattice(ilatt, 0) = std::stod(temp.substr(0, idx1)) / distanceBohrToAng;
+      int idx2 = temp.find(",", idx1 + 1);
+      lattice(ilatt, 1) =
+          std::stod(temp.substr(idx1 + 1, idx2)) / distanceBohrToAng;
+      lattice(ilatt, 2) = std::stod(temp.substr(idx2 + 1)) / distanceBohrToAng;
       ilatt++;
     }
-    if(line.find("lattice:") != std::string::npos) {
+    if (line.find("lattice:") != std::string::npos) {
       ilatt = 0;
     }
   }
@@ -129,12 +201,13 @@ Interaction3Ph IFC3Parser::parseFromPhono3py(Context &context, Crystal &crystal)
   std::cout << "check supercell positions" << std::endl;
 
   // convert positions to cartesian, in bohr
-  for(int i = 0; i<ipos; i++) {
-    Eigen::Vector3d temp(supPositions(i,0),supPositions(i,1),supPositions(i,2));
+  for (int i = 0; i < ipos; i++) {
+    Eigen::Vector3d temp(supPositions(i, 0), supPositions(i, 1),
+                         supPositions(i, 2));
     Eigen::Vector3d temp2 = lattice * temp;
-    supPositions(i,0) = temp2(0);
-    supPositions(i,1) = temp2(1);
-    supPositions(i,2) = temp2(2);
+    supPositions(i, 0) = temp2(0);
+    supPositions(i, 1) = temp2(1);
+    supPositions(i, 2) = temp2(2);
     std::cout << temp2(0) << " " << temp2(1) << " " << temp2(2) << std::endl;
   }
 
@@ -149,90 +222,105 @@ Interaction3Ph IFC3Parser::parseFromPhono3py(Context &context, Crystal &crystal)
   // set up buffer to read entire matrix
   // have to use this monstrosity because the phono3py data is shaped as a
   // 6 dimensional array, and eigen tensor is not supported by highFive
-  std::vector<std::vector<std::vector<std::vector<std::vector<std::vector<double>>>>>> ifc3Tensor;
+  std::vector<
+      std::vector<std::vector<std::vector<std::vector<std::vector<double>>>>>>
+      ifc3Tensor;
   std::vector<int> cellMap;
 
   // read in the ifc3 data
   difc3.read(ifc3Tensor);
-  dcellMap.read(cellMap); 
+  dcellMap.read(cellMap);
 
   // Determine the list of possible R2, R3 vectors
-  // the distances from the unit cell to a supercell 
-  Eigen::MatrixXd cellPositions2(3, nr);
-  Eigen::MatrixXd cellPositions3(3, nr);
-  cellPositions2.setZero();
-  cellPositions3.setZero();
-  for (int is = 0; is < nr; is++) { 
-
-      // find all possible vectors R2, R3, which are 
-      // position of atomPosSupercell - atomPosUnitCell = R
-      cellPositions2(0,is) = supPositions(is,0) - supPositions(0,0);
-      cellPositions2(1,is) = supPositions(is,1) - supPositions(0,1);
-      cellPositions2(2,is) = supPositions(is,2) - supPositions(0,2);
-
-      cellPositions3(0,is) = supPositions(is,0) - supPositions(0,0);
-      cellPositions3(1,is) = supPositions(is,1) - supPositions(0,1);
-      cellPositions3(2,is) = supPositions(is,2) - supPositions(0,2);
-
+  // the distances from the unit cell to a supercell
+  Eigen::MatrixXd supBravaisVectors(3, numSupCells);
+  supBravaisVectors.setZero();
+  for (int is = 0; is < numSupCells; is++) {
+    // find all possible vectors R2, R3, which are
+    // position of atomPosSupercell - atomPosUnitCell = R
+    supBravaisVectors(0, is) = supPositions(is, 0) - supPositions(0, 0);
+    supBravaisVectors(1, is) = supPositions(is, 1) - supPositions(0, 1);
+    supBravaisVectors(2, is) = supPositions(is, 2) - supPositions(0, 2);
   }
 
-  // reshape to D3 format
-  Eigen::Tensor<double, 5> D3(numBands, numBands, numBands, nr, nr);
-  D3.setZero();
+  auto t1 = buildWignerSeitz(supBravaisVectors);
+  Eigen::MatrixXd supWSBravaisVectors = std::get<0>(t1);
+  Eigen::VectorXi wsMap = std::get<1>(t1);
 
-  // For the first atom index, we only go over atoms in the unit cell, 
-  // as we consider R1 = 0. 
-  double conversion = pow(distanceBohrToAng, 3) / energyRyToEv;
-  for (int ia1 = 0; ia1 < numAtoms; ia1++) {
-  
-    // loop over R2 and R3 atom triplets
-    for ( int isa2 = 0; isa2<numSupAtoms; isa2++) {
-      for ( int isa3 = 0; isa3<numSupAtoms; isa3++) {
+  int numSupWsBravaisVectors = supWSBravaisVectors.rows();
 
-      int ia2 = floor(isa2 / numAtoms);
+  //------------------------------------------------------------------------
+
+  // ifc3 Tensor has dimensions [numSupAtoms,numSupAtoms,numSupAtoms,3, 3, 3]
+  Eigen::Tensor<double, 8> tmpTensor(numSupCells, numSupCells, numAtoms,
+                                     numAtoms, numAtoms, 3, 3, 3);
+  tmpTensor.setZero();
+  for (int isa2 = 0; isa2 < numSupAtoms; isa2++) {
+    int ia2 = floor(isa2 / numAtoms);
+    for (int isa3 = 0; isa3 < numSupAtoms; isa3++) {
       int ia3 = floor(isa3 / numAtoms);
 
-        // loop over cartesian coordinates
-        for (int ic1 : { 0, 1, 2 }) {
-          for (int ic2 : { 0, 1, 2 }) {
-            for (int ic3 : { 0, 1, 2 }) {
+      // TODO: we could avoid a search
+      // index of r2, r3 in the list of cellPositions
+      Eigen::Vector3d r2, r3;
+      for (int i : {0, 1, 2}) {
+        r2(i) = supPositions(isa2, i) - supPositions(cellMap[ia2], i);
+        r3(i) = supPositions(isa3, i) - supPositions(cellMap[ia3], i);
+      }
+      auto ir2 = findIndexRow(supBravaisVectors, r2);
+      auto ir3 = findIndexRow(supBravaisVectors, r3);
 
-              // compress the cartesian indices and unit cell atom indices 
-              // for the first three indices of D3
-              auto ind1 = compress2Indeces(ia1, ic1, numAtoms, 3);
-              auto ind2 = compress2Indeces(ia2, ic2, numAtoms, 3);
-              auto ind3 = compress2Indeces(ia3, ic3, numAtoms, 3);              
+      for (int isa1 = 0; isa1 < numSupAtoms; isa1++) {
+        int ia1 = floor(isa1 / numAtoms);
 
-              // index of r2, r3 in the list of cellPositions
-              Eigen::Vector3d r2, r3;
-              r2(0) = supPositions(isa2,0) - supPositions(cellMap[ia2],0);
-              r2(1) = supPositions(isa2,1) - supPositions(cellMap[ia2],1);
-              r2(2) = supPositions(isa2,2) - supPositions(cellMap[ia2],2);
-
-              r3(0) = supPositions(isa3,0) - supPositions(cellMap[ia3],0);
-              r3(1) = supPositions(isa3,1) - supPositions(cellMap[ia3],1);
-              r3(2) = supPositions(isa3,2) - supPositions(cellMap[ia3],2);
-
-              auto ir2 = findIndexRow(cellPositions2, r2);
-              auto ir3 = findIndexRow(cellPositions3, r3);
-
-              //std::cout << " ia1 ia2 ia3 isa2 isa3 " << ia1 << " " << ia2 << " " << ia3 << " " << isa2 << " " << isa3 << " ind1 ind2 ind3 ir2 ir3 " << ind1 << " " << ind2 << " " << ind3 << "  " << ir2 << " " << ir3 << " " << ifc3Tensor[cellMap[ia1]][isa2][isa3][ic1][ic2][ic3] << std::endl; 
-              //std::cout << " r3 " << r3(0) << " " << r3(1) << " " << r3(2) << std::endl; 
-              //std::cout << " cellPos3 " << cellPositions3(0,ir3) << " " << cellPositions3(1,ir3) << " " << cellPositions3(2,ir3) << std::endl; 
-
-              D3(ind1, ind2, ind3, ir2, ir3) // convert from ev/ang to atomic 
-                = ifc3Tensor[cellMap[ia1]][isa2][isa3][ic1][ic2][ic3] * conversion;
-
+        for (int i : {0, 1, 2}) {
+          for (int j : {0, 1, 2}) {
+            for (int k : {0, 1, 2}) {
+              tmpTensor(ir2, ir3, ia1, ia2, ia3, i, j, k) =
+                  ifc3Tensor[isa1][isa2][isa3][i][j][k];
             }
           }
         }
       }
     }
   }
+
+  // reshape to D3 format
+  Eigen::Tensor<double, 5> D3(numBands, numBands, numBands,
+                              numSupWsBravaisVectors, numSupWsBravaisVectors);
+  D3.setZero();
+  for (int ind1 = 0; ind1 < numBands; ind1++) {
+    auto t1 = decompress2Indeces(ind1, numAtoms, 3);
+    int ia1 = std::get<0>(t1);
+    int ic1 = std::get<1>(t1);
+    for (int ind2 = 0; ind2 < numBands; ind2++) {
+      auto t2 = decompress2Indeces(ind2, numAtoms, 3);
+      int ia2 = std::get<0>(t2);
+      int ic2 = std::get<1>(t2);
+      for (int ind3 = 0; ind3 < numBands; ind3++) {
+        auto t3 = decompress2Indeces(ind3, numAtoms, 3);
+        int ia3 = std::get<0>(t3);
+        int ic3 = std::get<1>(t3);
+        for (int irws2=0; irws2<numSupWsBravaisVectors; irws2++) {
+          for (int irws3 = 0; irws3 < numSupWsBravaisVectors; irws3++) {
+            int ir2 = wsMap(irws2);
+            int ir3 = wsMap(irws3);
+            D3(ind1, ind2, ind3, irws2, irws3) =
+                tmpTensor(ir2, ir3, ia1, ia2, ia3, ic1, ic2, ic3);
+          }
+        }
+      }
+    }
+  }
+  tmpTensor.resize(0,0,0,0,0,0,0,0);
+
+  double conversion = pow(distanceBohrToAng, 3) / energyRyToEv;
+  D3 *= conversion;
+
   if(mpi->mpiHead()) std::cout << "Done reading in D3.\n" << std::endl;
 
   // Create interaction3Ph object
-  Interaction3Ph interaction3Ph(crystal, D3, cellPositions2, cellPositions3);
+  Interaction3Ph interaction3Ph(crystal, D3, supWSBravaisVectors, supWSBravaisVectors);
 
   return interaction3Ph;
 
--- src/parser/ifc3_parser.h
+++ src/parser/ifc3_parser.h
@@ -1,51 +1,81 @@
-#include "interaction_3ph.h"
 #include "context.h"
 #include "crystal.h"
+#include "interaction_3ph.h"
 
 /** Class for the parsing of the 3rd derivative of the total energy with
  * respect to ionic displacements, and the creation of a dedicated object.
  */
 class IFC3Parser {
 public:
-    /** Interface to parse the 3rd derivative files.
-     * Will open the file and decide whether it's a ShengBTE or QuantumESPRESSO
-     * format, and call the dedicated method.
-     * This is the method that should be called in the rest of the code.
-     * @param crystal: Crystal object. To be obtained parsing the dynamical
-     * matrix first.
-     * @param context: the Context object with the user input (to get filename)
-     * @return Interaction3Ph: the object that contains the 3rd derivative.
-     */
-    static Interaction3Ph parse(Context &context, Crystal &crystal);
+  /** Interface to parse the 3rd derivative files.
+   * Will open the file and decide whether it's a ShengBTE or QuantumESPRESSO
+   * format, and call the dedicated method.
+   * This is the method that should be called in the rest of the code.
+   * @param crystal: Crystal object. To be obtained parsing the dynamical
+   * matrix first.
+   * @param context: the Context object with the user input (to get filename)
+   * @return Interaction3Ph: the object that contains the 3rd derivative.
+   */
+  static Interaction3Ph parse(Context &context, Crystal &crystal);
 
-    /** Parse the 3rd derivative of the total energy wrt ionic displacements.
-     * using the ShengBTE format.
-     * @param crystal: Crystal object. To be obtained parsing the dynamical
-     * matrix first.
-     * @param context: the Context object with the user input (to get filename)
-     * @return Interaction3Ph: the object that contains the 3rd derivative.
-     */
-    static Interaction3Ph parseFromShengBTE(Context &context, Crystal &crystal);
+  /** Parse the 3rd derivative of the total energy wrt ionic displacements.
+   * using the ShengBTE format.
+   * @param crystal: Crystal object. To be obtained parsing the dynamical
+   * matrix first.
+   * @param context: the Context object with the user input (to get filename)
+   * @return Interaction3Ph: the object that contains the 3rd derivative.
+   */
+  static Interaction3Ph parseFromShengBTE(Context &context, Crystal &crystal);
 
-    /** Parse the 3rd derivative of the total energy wrt ionic displacements.
-     * using the Phono3py format.
-     * @param context: the Context object with the user input (to get filename)
-     * @return Interaction3Ph: the object that contains the 3rd derivative.
-     */
-    static Interaction3Ph parseFromPhono3py(Context &context, Crystal &crystal);
+  /** Parse the 3rd derivative of the total energy wrt ionic displacements.
+   * using the Phono3py format.
+   * @param context: the Context object with the user input (to get filename)
+   * @return Interaction3Ph: the object that contains the 3rd derivative.
+   */
+  static Interaction3Ph parseFromPhono3py(Context &context, Crystal &crystal);
 
-    /** In development!
-     * TODO: while the parsing is done, and it's easy, QE uses a different
-     * convention. Let D3(r1,r2,r3) be the 3rd derivative matrix (cartesian and
-     * atomic indices excluded for simplicity). ShengBTE defines D3 such that
-     * r1 is always in the origin. QE instead assumes that r3 is in the origin.
-     * Thus, one should either (1) modify interaction3Ph to put the suitable
-     * phases, or (2) Fourier transform D3(r1,r2,r3) -> D3(q1,q2,q3) and then
-     * transform back setting r1=0.
-     * @param crystal: Crystal object. To be obtained parsing the dynamical
-     * matrix first.
-     * @param context: the Context object with the user input (to get filename)
-     * @return Interaction3Ph: the object that contains the 3rd derivative.
-     */
-    static Interaction3Ph parseFromQE(Context &context, Crystal &crystal);
+  /** In development!
+   * TODO: while the parsing is done, and it's easy, QE uses a different
+   * convention. Let D3(r1,r2,r3) be the 3rd derivative matrix (cartesian and
+   * atomic indices excluded for simplicity). ShengBTE defines D3 such that
+   * r1 is always in the origin. QE instead assumes that r3 is in the origin.
+   * Thus, one should either (1) modify interaction3Ph to put the suitable
+   * phases, or (2) Fourier transform D3(r1,r2,r3) -> D3(q1,q2,q3) and then
+   * transform back setting r1=0.
+   * @param crystal: Crystal object. To be obtained parsing the dynamical
+   * matrix first.
+   * @param context: the Context object with the user input (to get filename)
+   * @return Interaction3Ph: the object that contains the 3rd derivative.
+   */
+  static Interaction3Ph parseFromQE(Context &context, Crystal &crystal);
+
+private:
+  /** Find the index of a 3d vector in a matrix of 3d vectors.
+   * Calls an error if the vector is not found
+   *
+   * @param cellPositions2: the matrix containing the list of vectors to be
+   * searched, of size (numVectors,3)
+   * @param position2: the target of the search algorithm
+   * @return idx: the row index of position2 in cellPositions2
+   */
+  long findIndexRow(Eigen::MatrixXd &cellPositions2,
+                    Eigen::Vector3d &position2);
+
+  /** This function, used for phonon3py, folds a list of Bravais vectors into
+   * the Wigner Seitz zone of a supercell.
+   *
+   * @param supBravaisVectors: list of Bravais lattice vectors, in cartesian
+   * coordinates, which is assumed to be in the form i*a1+j*a2+k*a3, with
+   * a1,a2,a3 the primitive lattice vectors, and i,j,k NON-NEGATIVE integers
+   * @param crystal: Crystal object describing the primitive unit cell
+   * @param grid: identifies the size of the supercell, compared to the
+   * primitive unit cell
+   * @return a tuple with: a matrix of size (numWSVectors,3) containing the list
+   * of Bravais lattice vectors folded in the Wigner Seitz zone, containing also
+   * reducible copies; a map as a Eigen::VectorXi such that map(wsIndex) maps
+   * to the equivalent lattice vector in the input list.
+   */
+  std::tuple<Eigen::MatrixXd, Eigen::VectorXi>
+  buildWignerSeitz(const Eigen::MatrixXd &supBravaisVectors, Crystal &crystal,
+                   Eigen::Vector3i grid);
 };
