053645e49aa2374f67bf4cab7a35d1c75cddc9c5
jcoulter12 <jcoulter@g.harvard.edu>
2021-03-09 23:44:41 -0500

    add date and time to printed output

--- src/mpi/mpiController.cpp
+++ src/mpi/mpiController.cpp
@@ -39,19 +39,23 @@ MPIcontroller::MPIcontroller() {
 }
 
 void MPIcontroller::finalize() const {
+  if(mpiHead()) {
+    // print date and time of run
+    auto timenow = std::chrono::system_clock::to_time_t(
+          std::chrono::system_clock::now());
+    std::cout << "Finished on " << ctime(&timenow);
+  }
 #ifdef MPI_AVAIL
   barrier();
   if (mpiHead()) {
-    fprintf(stdout, "Final time: %3f\n s", MPI_Wtime() - startTime);
+    fprintf(stdout, "Run time: %3f s\n", MPI_Wtime() - startTime);
   }
   MPI_Finalize();
 #else
-  std::cout << "Final time: "
+  std::cout << "Run time: "
             << std::chrono::duration_cast<std::chrono::microseconds>(
-                   std::chrono::steady_clock::now() - startTime)
-                       .count() *
-                   1e-6
-            << " s" << std::endl;
+               std::chrono::steady_clock::now() - startTime)
+                  .count() * 1e-6 << " s" << std::endl;
 #endif
 }
 
