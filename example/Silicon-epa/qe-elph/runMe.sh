8b37e154352e05e1e2f31048271d05469be05d43
jcoulter12 <jcoulter@g.harvard.edu>
2021-04-16 23:46:19 -0400

    remove unnecessary nscf run from epa folder

--- example/Silicon-epa/qe-elph/runMe.sh
+++ example/Silicon-epa/qe-elph/runMe.sh
@@ -10,4 +10,3 @@ export NPOOL=4
 mpirun -np $NMPI $QE_PATH/pw.x -npool $NPOOL -in scf.in > scf.out
 mpirun -np $NMPI $QE_PATH/ph.x -npool $NPOOL -in ph.in > ph.out
 $QE_PATH/q2r.x -in q2r.in > q2r.out
-mpirun -np $NMPI $QE_PATH/pw.x -npool $NPOOL -in nscf.in > nscf.out
