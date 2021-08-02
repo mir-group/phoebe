83b978d0807cc2272508dacbe0a10cbf4dcecf55
jcoulter12 <jcoulter@g.harvard.edu>
2021-08-02 11:06:12 -0400

    removed error.what call that seemed to prevent jobs from ending properly after failing to write elph files

--- src/apps/qe_to_phoebe_wannier.cpp
+++ src/apps/qe_to_phoebe_wannier.cpp
@@ -1677,7 +1677,6 @@ void ElPhQeToPhoebeApp::writeWannierCoupling(
       dElDegeneracies.write(elDegeneracies);
     }
   } catch (std::exception &error) {
-    std::cout << error.what() << std::endl;
     Error("Issue writing elph Wannier representation to hdf5.");
   }
 
