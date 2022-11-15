39423215286e48d889da785bb20e531b2f30e441
jcoulter12 <jcoulter@g.harvard.edu>
2022-11-15 16:51:55 -0500

    add in index information to conversion script

--- scripts/convertHDF5.py
+++ scripts/convertHDF5.py
@@ -3,7 +3,7 @@ import h5py
 import os
 import glob
 
-for filename in glob.glob('data/QP_BSE/ndb.BS*'):
+for filename in glob.glob('oldData/QP_BSE/ndb.BS*'):
 
   if("hdf5" in filename or ("PAR_Q" not in filename and "head_Q1" not in filename)):
     continue
@@ -11,6 +11,7 @@ for filename in glob.glob('data/QP_BSE/ndb.BS*'):
   # creating a file
   oldFile = h5py.File(filename, 'r')
   newFile = h5py.File(filename+'.hdf5','w')
+  print(oldFile.keys())
 
   print(filename)
 
@@ -21,6 +22,9 @@ for filename in glob.glob('data/QP_BSE/ndb.BS*'):
 
       # open the data
       data = np.array(list(oldFile[key]))
+      #print(key)
+      #print(data)
+      #print(oldFile[key])
       if(key == 'Bands'):
         data = data.astype(np.int)
         data = np.array([[data[0],data[1]]])
@@ -32,13 +36,23 @@ for filename in glob.glob('data/QP_BSE/ndb.BS*'):
 
   elif ("PAR_Q" in filename):
 
-    for ik, key in enumerate(['BSE_RESONANT']):
+    for ik, key in enumerate(['BSE_RESONANT','IKBZ_IB1_IB2_ISP2_ISP1']):
 
       # open the data
       data = np.array(list(oldFile[key]))
+      #print(key)
+      #print(data)
+      #print(oldFile[key])
       if(key == 'BSE_RESONANT'):
         data = 1j*data[...,1] + data[...,0]
+        #data = data.astype(np.complex128)
+        #data = np.array(data)
+        #size = (2)
+      #if(key == 'HEAD_QPT'):
+      #  data = data.astype(np.float64)
+      #  size = data.shape
       newFile.create_dataset(key, data=data);
+      #print(d)
 
   newFile.close()
   oldFile.close()
