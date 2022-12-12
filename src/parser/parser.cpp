7e05e9b96fbf58009a0e7b9458a4d1173cbc33ac
jcoulter12 <jcoulter@g.harvard.edu>
2022-12-12 15:21:00 -0500

    extra error message for if the QE fc parser is used on a file named HDF5, which would indicate a problem

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -6,6 +6,14 @@ std::tuple<Crystal, PhononH0> Parser::parsePhHarmonic(Context &context) {
 
   // check if this file is set -- if it is, we read from phonopy
   if (fileName.empty()) {
+    if(context.getPhFC2FileName().find("hdf5") != std::string::npos) {
+      // Note for the when someone forgets to set PhonopyDispFileName and gets an stoi error
+      if(mpi->mpiHead()) {
+        std::cout << "Warning: Reading in a QE .fc file with a name containing hdf5." << '\n' <<
+        "If you are trying to use Phonopy force constants," <<
+        " make sure to set the PhonopyDispFileName variable." << std::endl;
+        }
+    }
     return QEParser::parsePhHarmonic(context);
   }
   else {
