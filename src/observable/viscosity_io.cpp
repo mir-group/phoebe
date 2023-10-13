a74a781824347f04d4cd48b4f5e3f76c9931b116
jcoulter12 <jcoulter@g.harvard.edu>
2023-10-13 16:57:53 -0400

    add a generic funciton to write D and W to file

--- src/observable/viscosity_io.cpp
+++ src/observable/viscosity_io.cpp
@@ -47,7 +47,7 @@ void printViscosity(std::string& viscosityName, Eigen::Tensor<double, 5>& viscos
 
 
 void outputViscosityToJSON(const std::string& outFileName, const std::string& viscosityName,
-                Eigen::Tensor<double, 5>& viscosityTensor, const bool& isPhonon, const bool& append,
+                Eigen::Tensor<double, 5>& viscosityTensor, const bool& append,
                 StatisticsSweep& statisticsSweep, int& dimensionality) {
 
   if (!mpi->mpiHead()) return;
@@ -105,3 +105,91 @@ void outputViscosityToJSON(const std::string& outFileName, const std::string& vi
   o << std::setw(3) << output << std::endl;
   o.close();
 }
+
+void genericOutputRealSpaceToJSON(ScatteringMatrix& scatteringMatrix,
+                                BaseBandStructure& bandStructure,
+                                StatisticsSweep& statisticsSweep,
+                                Eigen::VectorXd& theta0,
+                                Eigen::VectorXd& theta_e,
+                                Eigen::MatrixXd& phi) {
+
+  // write D to file before diagonalizing, as the scattering matrix
+  // will be destroyed by scalapack
+
+  bool isPhonon = bandStructure.getParticle().isPhonon();
+
+  auto calcStat = statisticsSweep.getCalcStatistics(0); // only one calc for relaxons
+  double kBT = calcStat.temperature;
+
+  Eigen::MatrixXd Du(3,3); Du.setZero();
+  Eigen::MatrixXd Wji0(3,3); Wji0.setZero();
+  // below used only for electrons
+  Eigen::MatrixXd Wjie(3,3); Wjie.setZero();
+
+  // sum over the alpha and v states that this process owns
+  for (auto tup : scatteringMatrix.getAllLocalStates()) {
+
+    auto is1 = std::get<0>(tup);
+    auto is2 = std::get<1>(tup);
+    for (auto i : {0, 1, 2}) {
+      for (auto j : {0, 1, 2}) {
+        Du(i,j) += phi(i,is1) * scatteringMatrix(is1,is2) * phi(j,is2) * energyRyToEv;
+      }
+    }
+  }
+  mpi->allReduceSum(&Du);
+
+  for (int is : bandStructure.parallelStateIterator()) {
+    auto isIdx = StateIndex(is);
+    double en = bandStructure.getEnergy(isIdx);
+    // discard acoustic phonon modes
+    if (en < 0.001 / ryToCmm1) { continue; }
+    auto v = bandStructure.getGroupVelocity(isIdx);
+    for (auto j : {0, 1, 2}) {
+      for (auto i : {0, 1, 2}) {
+        // note: phi and theta here are elStates long, so we need to shift the state
+        // index to account for the fact that we summed over the electronic part above
+        // calculate qunatities for the real-space solve
+        Wji0(j,i) += phi(i,is) * v(j) * theta0(is) * velocityRyToSi;
+        Wjie(j,i) += phi(i,is) * v(j) * theta_e(is) * velocityRyToSi;
+      }
+    }
+  }
+  mpi->allReduceSum(&Wji0); mpi->allReduceSum(&Wjie);
+
+  // NOTE we cannot use nested vectors from the start, as
+  // vector<vector> is not necessarily contiguous and MPI
+  // cannot all reduce on it
+  std::vector<std::vector<double>> vecDu;
+  std::vector<std::vector<double>> vecWji0;
+  std::vector<std::vector<double>> vecWjie;
+  for (auto i : {0, 1, 2}) {
+    std::vector<double> temp1,temp2,temp3;
+    for (auto j : {0, 1, 2}) {
+      temp1.push_back(Du(i,j));
+      temp2.push_back(Wji0(i,j));
+      temp3.push_back(Wjie(i,j));
+    }
+    vecDu.push_back(temp1);
+    vecWji0.push_back(temp2);
+    vecWjie.push_back(temp3);
+  }
+
+  if(mpi->mpiHead()) {
+    // output to json
+    std::string outFileName = "electron_relaxons_real_space_coeffs.json";
+    if(isPhonon) outFileName = "phonon_relaxons_real_space_coeffs.json";
+    nlohmann::json output;
+    output["temperature"] = kBT * temperatureAuToSi;
+    output["Wji0"] = vecWji0;
+    if(!isPhonon) output["Wjie"] = vecWjie;
+    output["Du"] = vecDu;
+    output["temperatureUnit"] = "K";
+    output["wUnit"] = "m/s";
+    output["DuUnit"] = "eV";
+    std::ofstream o(outFileName);
+    o << std::setw(3) << output << std::endl;
+    o.close();
+  }
+}
+
--- src/observable/viscosity_io.h
+++ src/observable/viscosity_io.h
@@ -1,6 +1,6 @@
 #ifndef VISCOSITY_IO_H
 #define VISCOSITY_IO_H
-#include "statistics_sweep.h"
+#include "scattering.h"
 
   /** Prints the viscosity tensor to std out
    * @param viscosityName: string which is used to print the name of the viscosity tensor
@@ -12,13 +12,27 @@
    * @param outFileName: string representing the name of the json file
    * @param viscosityName: string which is used to as the key in the json file for viscosity tensor
    * @param viscosityTensor: a tensor with indices (iCalc,i,j,k,l)
-   * @param isPhonon : boolean to tell if this is a phonon, needed because phonons need an extra 2pi factor
    * @param append : boolean to tell if this should be appended to an existing file
    * @param statisticsSweep: object containing temperature, chemPot, etc info
    * @param dimensionality: the dimension of the crystal
    */
    void outputViscosityToJSON(const std::string& outFileName, const std::string& viscosityName,
-                Eigen::Tensor<double, 5>& viscosityTensor, const bool& isPhonon, const bool& append,
+                Eigen::Tensor<double, 5>& viscosityTensor, const bool& append,
                 StatisticsSweep& statisticsSweep, int& dimensionality);
+
+  /** Outputs the viscosity to a json file.
+   * @param outFileName: string representing the name of the json file
+   * @param bandStructure: bandstructure for either phonons or electrons
+   * @param statisticsSweep: object with temperatures, chemical potentials, etc
+   * @param theta0: energy conservation eigenvector
+   * @param thetae: charge conservation eigenvector
+   * @param phi: momentum conservation eigenvectors
+   */
+   void genericOutputRealSpaceToJSON(ScatteringMatrix& scatteringMatrix,
+                                BaseBandStructure& bandStructure,
+                                StatisticsSweep& statisticsSweep,
+                                Eigen::VectorXd& theta0,
+                                Eigen::VectorXd& theta_e,
+                                Eigen::MatrixXd& phi);
 
 #endif
