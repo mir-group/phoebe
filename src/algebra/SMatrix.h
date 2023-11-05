1a1a93d08e93d1acdd20e508fd2384c82f86f354
jcoulter12 <jcoulter@g.harvard.edu>
2023-11-05 15:33:51 -0500

    add more information to error message for out of bounds access in SMatrix

--- src/algebra/SMatrix.h
+++ src/algebra/SMatrix.h
@@ -287,7 +287,7 @@ int SerialMatrix<T>::size() const {
 template <typename T>
 T& SerialMatrix<T>::operator()(const int &row, const int &col) {
   if(row >= numRows_ || col >= numCols_ || row < 0 || col < 0) {
-    Error("Attempted to reference a matrix element out of bounds.");
+    DveloperError("SMatrix get/set attempted to reference a matrix element out of bounds.");
   }
   return mat[global2Local(row, col)];
 }
@@ -295,7 +295,7 @@ T& SerialMatrix<T>::operator()(const int &row, const int &col) {
 template <typename T>
 const T& SerialMatrix<T>::operator()(const int &row, const int &col) const {
   if(row >= numRows_ || col >= numCols_ || row < 0 || col < 0) {
-    Error("Attempted to reference a matrix element out of bounds.");
+    DeveloperError("SMatrix get/set attempted to reference a matrix element out of bounds.");
   }
   return mat[global2Local(row, col)];
 }
