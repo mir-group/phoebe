e501b8b06caf05e8c79e427605e9b6f8d7381e90
jcoulter12 <jcoulter@flatironinstitute.org>
2024-10-24 13:59:12 -0400

    try custom reduction on eigen tensor

--- src/harmonic/phonon_h0.cpp
+++ src/harmonic/phonon_h0.cpp
@@ -786,14 +786,16 @@ void PhononH0::shortRangeTerm(Eigen::Tensor<std::complex<double>, 4> &dyn,
     phases[iR] = exp(-complexI * arg); // {cos(arg), -sin(arg)};
   }
 
-#pragma omp parallel for collapse(5) default(none) shared(dyn, mat2R, phases, weights)
+#pragma omp declare reduction (+: Eigen::Tensor<std::complex<double>, 4>: omp_out=omp_out+omp_in)\
+     initializer(omp_priv=Eigen::Tensor<std::complex<double>, 4>(omp_orig.dimension(0),omp_orig.dimension(1),omp_orig.dimension(2),omp_orig.dimension(3)).setZero())
+
+#pragma omp parallel for collapse(5) default(none) reduction (+:dyn) shared(mat2R, phases, weights,numAtoms,numBravaisVectors)
   for (int iR = 0; iR < numBravaisVectors; iR++) {
     for (int nb = 0; nb < numAtoms; nb++) {
       for (int na = 0; na < numAtoms; na++) {
         for (int j : {0, 1, 2}) {
           for (int i : {0, 1, 2}) {
-            dyn(i, j, na, nb) +=
-                mat2R(i, j, na, nb, iR) * phases[iR] * weights(iR);
+            dyn(i, j, na, nb) +=  mat2R(i, j, na, nb, iR) * phases[iR] * weights(iR);
           }
         }
       }
